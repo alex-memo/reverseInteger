75bc4905e862aa1cfe8617ef3603023481d0532d
memo <alexis45645@gmail.com>
2022-10-30 21:50:16 -0400

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(reverse(-645));
+		System.out.println(reverse(-645));//replace -645 with the desired number
 	}
 	public static int reverse(int x) {
 		String temp=Integer.toString(x);
